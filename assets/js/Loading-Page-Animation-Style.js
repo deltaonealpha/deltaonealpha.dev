6393ede5ed99399e8aacbf87b7b273d6b6823fed
deltaonealpha <balaji.pranav@outlook.in>
2022-03-10 05:55:39 +0530

    New design version

--- assets/js/Loading-Page-Animation-Style.js
+++
@@ -1,8 +0,0 @@
-window.onload = function () {
-    var de = document.getElementById("ldiv-1");
-    var delayInMilliseconds = 2000; //1 second
-
-setTimeout(function() {
-  de.style.display = 'none';
-}, delayInMilliseconds);
-};
